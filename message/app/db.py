9aad29a3c56d10800b45a39baf4de11ef6b1d653
xx529 <575506806@qq.com>
2022-10-14 23:29:06 +0800

    add db to message server

---
+++ message/app/db.py
@@ -0,0 +1,32 @@
+import os
+import psycopg2
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+conn_info = {'dbname': os.environ.get('PG_DB'),
+             'user': os.environ.get('PG_USER'),
+             'password': os.environ.get('PG_PASSWORD'),
+             'host': os.environ.get('PG_HOST'),
+             'port': os.environ.get('PG_PORT')}
+
+
+def auto_upload(func):
+    def inner(*args, **kwargs):
+        with psycopg2.connect(**conn_info) as conn:
+            sql = func(*args, **kwargs)
+            conn.cursor().execute(sql)
+        return True
+    return inner
+
+
+@auto_upload
+def add_message(data):
+    logging.info(
+        f"{data['wechat_id']} - {data['wechat_name']} - {data['room_id']} - {data['timestamp']} - {data['message'][:15]}...")
+
+    sql = f"""insert into message 
+              (wechat_id, wechat_name, room_id, timestamp, message)
+              values ('{data['wechat_id']}', '{data['wechat_name']}', '{data['room_id']}', {data['timestamp']}, '{data['message']}')
+               """
+    return sql
--- message/app/start.py
+++ message/app/start.py
@@ -1,31 +1,14 @@
 from flask import Flask, request
-import logging
-import os
 import json
-import psycopg2
+from db import add_message
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.INFO)
 
 
 @app.route('/message', methods=['POST'])
 def message():
     data = json.loads(request.json)
-    logging.info(f"{data['wechat_id']} - {data['wechat_name']} - {data['room_id']} - {data['timestamp']} - {data['message'][:15]}...")
-
-    with psycopg2.connect(dbname=os.environ.get('PG_DB'),
-                          user=os.environ.get('PG_USER'),
-                          password=os.environ.get('PG_PASSWORD'),
-                          host=os.environ.get('PG_HOST'),
-                          port=os.environ.get('PG_PORT')
-                         ) as conn:
-
-        sql = f"""insert into message 
-                 (wechat_id, wechat_name, room_id, timestamp, message)
-                 values ('{data['wechat_id']}', '{data['wechat_name']}', '{data['room_id']}', {data['timestamp']}, '{data['message']}')
-               """
-        conn.cursor().execute(sql)
-
+    add_message(data)
     return {'state': 'ok'}
 
 
